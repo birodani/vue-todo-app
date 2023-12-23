820589591f9f3964668785c6ab230843a30550c6
birodani <birodani91@gmail.com>
2023-12-23 12:09:59 +0100

    chore: modify tailwind configuration add new font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["*.vue"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme')
 
+export default {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                noto: ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
+                poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+}
